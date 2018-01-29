5bf35f802e1651f9e8f6adae9d3b9eec97c10ff7
Joshua Noble <acejam@gmail.com>
2018-01-29 17:51:09 -0500

    Update logging and entrypoint (#5)
    * Update entrypoint
    
    * Remove longs from Docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 
-echo "server=1
-maxconnections=${MAX_CONNECTIONS}
-rpcuser=${RPC_USER}
-rpcpassword=${RPC_PASS}
-rpcallowip=${RPC_ALLOW_IP}
-rpcport=${RPC_PORT}
-port=${PORT}
-txindex=1" > /data/namecoin/namecoin.conf
+if [ ! -f /data/namecoin/namecoin.conf ]; then
+  echo "server=1
+  maxconnections=${MAX_CONNECTIONS}
+  rpcuser=${RPC_USER}
+  rpcpassword=${RPC_PASS}
+  rpcallowip=${RPC_ALLOW_IP}
+  rpcport=${RPC_PORT}
+  port=${PORT}
+  txindex=1" > /data/namecoin/namecoin.conf
+fi
 
 exec "$@"
